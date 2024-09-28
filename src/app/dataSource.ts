6b99451a29bc03fc3c520a2752450773a07d6df7
진하늘 <hnjin@citus.co.kr>
2024-09-29 08:58:23 +0900

    data source dir 미스매칭 해결

--- src/app/dataSource.ts
+++ src/app/dataSource.ts
@@ -1,4 +1,7 @@
 import { DataSource } from "typeorm";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(import.meta.url);
 
 const AppDateSource = new DataSource({
   type: "mariadb",
@@ -9,7 +12,7 @@ const AppDateSource = new DataSource({
   database: "tennis",
   synchronize: true,
   logging: process.env.NODE_ENV === "development",
-  entities: ["src/**/entities/*.entity{.ts,.js}"],
+  entities: [`${__dirname}/**/entities/*.entity.{ts,js}`],
 });
 
 export default AppDateSource;
